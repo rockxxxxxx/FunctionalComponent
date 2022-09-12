0b13a08a82e1a7ce352b84fed1f485f987361cb5
Rakesh Raushan <raushanrakesh069@gmail.com>
2022-09-13 02:00:31 +0530

    Fixed the issue of delay in first time rendering while usiing settimeout function to delay api call

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -21,15 +21,19 @@ export default function Search() {
       setResults(data.query.search);
     };
 
-    const timeoutId = setTimeout(() => {
-      if (term) {
-        search();
-      }
-    }, 1000);
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 1000);
 
-    return () => {
-      clearTimeout(timeoutId);
-    };
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
   }, [term]);
 
   const renderedResult = results.map((result) => {
